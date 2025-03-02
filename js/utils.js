d23bee9062f219dc6de8bd537ab4a09afca91301
Julino Segunda Dídimo <ddimojulino@gmail.com>
2025-03-02 14:51:57 -0300

    bg button

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,7 @@
+const sender = document.querySelector('.whatsapp')
+sender.addEventListener('click', function () {
+    console.log(sender)
+})
 const dataFormat = () => {
     return new Intl.DateTimeFormat("pt-BR").format(new Date())
 }
