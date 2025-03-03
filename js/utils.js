72ff1c881eb02d2e409c4a28101d8977b9fb83d7
Julino Didimo <ddimojulino@gmail.com>
2025-03-03 17:44:43 -0300

    Página teste

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,4 @@
-const sender = document.querySelector('.whatsapp')
-sender.addEventListener('click', function () {
-    console.log(sender)
-})
+
 const dataFormat = () => {
     return new Intl.DateTimeFormat("pt-BR").format(new Date())
 }
