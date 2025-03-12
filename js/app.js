f895f4da5d878bcebae272fbb16901eb84ddc19e
Julino Segunda Dídimo <ddimojulino@gmail.com>
2025-03-12 14:06:12 -0300

    Javascript

--- js/app.js
+++ js/app.js
@@ -5,21 +5,24 @@ import dataFormat from "./utils.js";
 const siteList = document.querySelector('.sites')
 const init = async () => {
     const sites = await fetchSites()
-    siteList.innerHTML = sites.map((item) => {
-        const {
-            screenshot_url: image,
-            name: site_name, url: site_url,
-            updated_at: refresh,
-            account_slug: owner,
-            prerender: server,
-            build_settings,
-            admin_url,
-            published_deploy: deploy,
-        } = item
-        console.log(item)
-        const linkSite = deploy.links.alias
-        const provide = build_settings?.provider
-        return `
+    if (!sites) {
+        siteList.innerHTML = "<h1>Loading</h1>"
+    } else {
+        siteList.innerHTML = sites.map((item) => {
+            const {
+                screenshot_url: image,
+                name: site_name, url: site_url,
+                updated_at: refresh,
+                account_slug: owner,
+                prerender: server,
+                build_settings,
+                admin_url,
+                published_deploy: deploy,
+            } = item
+            console.log(item)
+            const linkSite = deploy.links.alias
+            const provide = build_settings?.provider
+            return `
             <li>
             <a href="${linkSite}" target="blank">
                 <figure>
@@ -39,7 +42,8 @@ const init = async () => {
             </a>
             </li>
         `
-    }).join(' ')
+        }).join(' ')
+    }
 
 }
 init()
